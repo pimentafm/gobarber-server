ee4306cebe95149bfb94b127806a53cbf205011e
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-06-07 20:46:30 -0300

    TEST: should be able to create a new appointmment

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentsService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointmment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+});
