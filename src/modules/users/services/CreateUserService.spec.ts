f710fa1c05ed8d0b41127aff9c5ca535b74410c0
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-06-07 21:41:58 -0300

    TESTE: Create user

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -16,4 +16,23 @@ describe('createUser', () => {
 
     expect(user).toHaveProperty('id');
   });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
